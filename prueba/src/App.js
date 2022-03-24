0875b6f7c6bd26b20d99d0bbf160f5c5814f7f96
Ayrton-Solis <95722681+Ayrton-Solis@users.noreply.github.com>
2022-03-24 20:22:03 -0300

    Mi primer commit de react

---
+++ prueba/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let primeravez = 'hola mundo ';
+  let pregunta = 'como estas?'
+  return (
+    <div className="App-header">
+      {primeravez}
+      {pregunta}
+    </div>
+  );
+}
+
+export default App;
